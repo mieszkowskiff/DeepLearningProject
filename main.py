5f704354854babad5f67f638d850ad5d84158185
mieszkowskiff <mieszkowskiff@gmail.com>
2025-06-05 20:32:48 +0200

    Update .gitignore and modify image classification project to handle additional file types and improve model architecture

--- main.py
+++ main.py
@@ -6,36 +6,28 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from model import InceptionCustom
 
-# === Parametry ===
-data_dir = "dataset"
+data_dir = "./dataset"
 batch_size = 32
 num_epochs = 10
 learning_rate = 1e-4
 num_classes = 2
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# === Transformacje ===
 transform = transforms.Compose([
     transforms.Resize((320, 320)),  # min. 299x299 dla Inception
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5]*3, std=[0.5]*3),
+    transforms.Normalize(mean = [0.5]*3, std = [0.5]*3),
 ])
 
-# === Dataset ===
-train_dataset = datasets.ImageFolder(root=data_dir, transform=transform)
+train_dataset = datasets.ImageFolder(root = data_dir, transform=transform)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
-# === Model ===
-from torchvision.models import inception_v3
 
 model = InceptionCustom(num_classes).to(device)
 
-# === Loss & Optymalizator ===
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-# === Pętla treningowa ===
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
--- model.py
+++ model.py
@@ -1,18 +1,20 @@
 import torch
 import torch.nn as nn
-from torchvision.models import inception_v3
+from torchvision.models import inception_v3, Inception_V3_Weights
+
+
 
 class InceptionCustom(nn.Module):
     def __init__(self, num_classes: int):
         super(InceptionCustom, self).__init__()
-        self.base = inception_v3(pretrained = True, aux_logits = False)
+        self.base = inception_v3(weights=Inception_V3_Weights.DEFAULT, aux_logits = True)
         self.base.fc = nn.Identity()  # Wywalamy klasyfikator
         self.pool = nn.AdaptiveAvgPool2d((1, 1))  # zabezpieczenie na różne rozdzielczości
         self.classifier = nn.Linear(2048, num_classes)
 
     def forward(self, x):
         x = self.base(x)  # super.forward()
-        # if isinstance(x, tuple):  # Czasem model może zwrócić tuple (output, aux_output)
-        #     x = x[0]
+        if isinstance(x, tuple):  # Czasem model może zwrócić tuple (output, aux_output)
+            x = x[0]
         x = self.classifier(x)
         return x
